5347c9b259547857daeacfa285f91e6dbe35b002
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-07-24 21:08:09 +0200

    ♻️ Introduce 'filename' field on Plugin

--- flake8_fastapi/plugin.py
+++ flake8_fastapi/plugin.py
@@ -1,4 +1,8 @@
+import ast
+from typing import Iterable
+
 from flake8_plugin_utils import Plugin
+from flake8_plugin_utils.plugin import FLAKE8_ERROR
 
 from flake8_fastapi import __version__
 from flake8_fastapi.visitors import (
@@ -18,3 +22,16 @@ class FastAPIPlugin(Plugin):
         GenericExceptionHandler,
         CORSMiddlewareOrder,
     ]
+
+    def __init__(self, tree: ast.AST, filename: str) -> None:
+        super().__init__(tree)
+        self._filename = filename
+
+    def run(self) -> Iterable[FLAKE8_ERROR]:
+        for visitor_cls in self.visitors:
+            visitor = self._create_visitor(visitor_cls)
+            visitor._filename = self._filename
+            visitor.visit(self._tree)
+
+            for error in visitor.errors:
+                yield self._error(error)
