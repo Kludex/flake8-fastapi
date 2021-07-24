4bc8602fb22c5f968a6513262002102d3078b74c
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-07-24 13:47:01 +0200

    ✨ Add CORSMiddlewareOrder rule

--- flake8_fastapi/plugin.py
+++ flake8_fastapi/plugin.py
@@ -1,10 +1,10 @@
 from flake8_plugin_utils import Plugin
 
 from flake8_fastapi import __version__
-from flake8_fastapi.visitors import RouteDecorator, RouterPrefix
+from flake8_fastapi.visitors import CORSMiddlewareOrder, RouteDecorator, RouterPrefix
 
 
 class FastAPIPlugin(Plugin):
     name = "flake8-fastapi"
     version = __version__
-    visitors = [RouteDecorator, RouterPrefix]
+    visitors = [RouteDecorator, RouterPrefix, CORSMiddlewareOrder]
