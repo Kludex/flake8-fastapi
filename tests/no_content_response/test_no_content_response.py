f3acec1f82b51f7d6d5cc7168a6f82491f8b3f51
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-07-25 06:50:35 +0200

    ♻️ Infer code parameter globally on the tests (#17)

--- tests/no_content_response/test_no_content_response.py
+++ tests/no_content_response/test_no_content_response.py
@@ -7,17 +7,9 @@ from flake8_fastapi.errors import NoContentResponseError
 from flake8_fastapi.visitors import NoContentResponse
 
 
-def test_code_with_error():
-    code = (Path(__file__).parent / "with_error.py").read_text()
+def test_code_with_error(code: str):
     assert_error(NoContentResponse, code, NoContentResponseError)
 
 
-@pytest.mark.parametrize(
-    "code",
-    [
-        (Path(__file__).parent / "without_error.py").read_text(),
-        (Path(__file__).parent / "without_error_return.py").read_text(),
-    ],
-)
 def test_code_without_error(code: str):
     assert_not_error(NoContentResponse, code)
--- tests/router_prefix/test_router_prefix.py
+++ tests/router_prefix/test_router_prefix.py
@@ -6,11 +6,9 @@ from flake8_fastapi.errors import RouterPrefixError
 from flake8_fastapi.visitors import RouterPrefix
 
 
-def test_code_with_error():
-    code = (Path(__file__).parent / "with_error.py").read_text()
+def test_code_with_error(code: str):
     assert_error(RouterPrefix, code, RouterPrefixError)
 
 
-def test_code_without_error():
-    code = (Path(__file__).parent / "without_error.py").read_text()
+def test_code_without_error(code: str):
     assert_not_error(RouterPrefix, code)
