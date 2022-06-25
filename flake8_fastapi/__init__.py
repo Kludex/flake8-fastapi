d300a46bb17958c1b85fa7c1d079b43b7ccc32c4
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-06-25 19:40:55 +0200

    Ignore mypy issue

--- flake8_fastapi/__init__.py
+++ flake8_fastapi/__init__.py
@@ -1,6 +1,6 @@
 try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
-    import importlib_metadata
+    import importlib_metadata  # type: ignore[no-redef]
 
 __version__ = importlib_metadata.version(__name__)
