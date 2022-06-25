1d3e72bbac562e1e130c8c6f3188b47de0c9f3ba
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-06-25 19:49:28 +0200

    Add pragma no cover

--- flake8_fastapi/__init__.py
+++ flake8_fastapi/__init__.py
@@ -1,6 +1,6 @@
 try:
     import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
+except ModuleNotFoundError:  # pragma: no cover
     import importlib_metadata  # type: ignore[no-redef]
 
 __version__ = importlib_metadata.version(__name__)
